8b4f8f773eaf78fb577e8bc465395cdddd0a0c03
christianscode1994 <fourieranalys@hotmail.com>
2025-11-19 22:18:54 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'es2020'
-  }
+export default defineConfig(async () => {
+  const reactPlugin = (await import('@vitejs/plugin-react')).default;
+  return {
+    plugins: [reactPlugin()],
+    build: { target: 'es2020' }
+  };
 });
